0480643215b8a089720ed7ce5b7b27c7f783754b
Martin Doege <mdoege@compuserve.com>
2017-08-21 11:00:19 +0200

    add __main__.py starting method
    can use "python 3NewsFeed" to start now

---
+++ __main__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Newsfeed wrapper script (like Start_NewsFeed.py, but without the extension)
+
+A Python/Tk RSS/RDF/Atom news aggregator. See included README.html for documentation.
+
+Martin Doege, 2013-01-10
+
+"""
+
+import sys
+
+assert sys.version >= '3.3', "Please install Python 3.3 or later."
+
+nogui = '--nogui' in sys.argv
+
+if __name__ == '__main__':
+	from newsfeed import *
+
+	try: main(nogui = nogui)
+	finally:
+		try: os.unlink(pid_file)
+		except: pass
+		save()
